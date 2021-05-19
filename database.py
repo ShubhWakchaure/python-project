a2cdc2e2f62c2cda3e2b783e1d20fe449eb3c8fc
Shubham <shubham30nw@gmail.com>
2021-05-19 14:55:32 +0530

    Initial commit

---
+++ database.py
@@ -0,0 +1,8 @@
+create table emp( 
+    empid int, 
+    empname varchar(50), 
+    empsalary int, 
+    empcity varchar(50) 
+ 
+ 
+);
---
+++ myprint.py
@@ -0,0 +1,25 @@
+'''
+Visit us for more free projects on
+our official website ApkaGuruji.com
+'''
+SCREEN_WIDTH = 100 
+
+
+def print_center(s): 
+    x_pos = SCREEN_WIDTH // 2
+    print((" " * x_pos), s)
+
+
+def print_bar(): 
+    print("=" * 100)
+
+
+def print_bar_ln(): 
+    print_bar()
+    print()
+
+
+def input_center(s): 
+    x_pos = SCREEN_WIDTH // 2
+    print((" " * x_pos), s, end='')
+    return input()
